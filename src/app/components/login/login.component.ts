24b4f0fbcb1d274a025c0f81c44e6681304e41a7
Hugh Carslaw <2314197c@student.gla.ac.uk>
2023-10-04 21:22:59 +0100

    Initial login page changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl('')
+  });
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
