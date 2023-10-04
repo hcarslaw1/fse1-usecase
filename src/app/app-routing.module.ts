a97b936a9f03ce98b73dd8e907b805406a4576c8
Hugh Carslaw <2314197c@student.gla.ac.uk>
2023-10-04 21:16:09 +0100

    Project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'view-products', component: ViewProductsComponent },
+  { path: 'update-products', component: UpdateProductComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'logout', component: LogoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ViewProductsComponent,
+    UpdateProductComponent,
+    AddProductComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
